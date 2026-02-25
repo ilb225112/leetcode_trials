5a74dae0be2c1fd99784d574c2da25375fd3f824
Chirag K M <pes1ug23cs167@pesu.pes.edu>
2026-02-26 01:24:18 +0530

    Time: 0 ms (100%), Space: 13.8 MB (47.52%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.c
+++ 0041-first-missing-positive/0041-first-missing-positive.c
@@ -1,15 +1,20 @@
 int firstMissingPositive(int* nums, int numsSize) {
+    //logic: soln is either <=numsSize or numsSize+1
+    //replace all -ve numbers with random +ve no.
     for(int i=0;i<numsSize;i++)
         if(nums[i]<=0)
             nums[i] = numsSize+1;
+    
+    //mark no.<numsSize ka pos -> -ve
     for(int i=0;i<numsSize;i++){
         int val=abs(nums[i]);
         if(val<=numsSize && nums[val-1]>0)
             nums[val-1]*=-1;
     }
+
+    //whichever positive no. is encountered 1st return that pos;
     for(int i=0;i<numsSize;i++)
         if(nums[i]>0)
             return i+1;
     return numsSize+1;
-
 }
