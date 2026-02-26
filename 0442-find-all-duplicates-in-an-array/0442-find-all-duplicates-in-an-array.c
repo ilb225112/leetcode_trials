8c23ca22e5c18ceda99d4e88d6a35d583db3bfad
Chirag K M <pes1ug23cs167@pesu.pes.edu>
2026-02-27 02:13:25 +0530

    Time: 0 ms (100%), Space: 24.3 MB (76.92%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.c
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.c
@@ -5,15 +5,11 @@ int* findDuplicates(int* nums, int numsSize, int* returnSize) {
     int* res =(int*)malloc(sizeof(int)*(numsSize/2 + 1));
     *returnSize=0;
 
-    int bitmap[(100001/32)+1]={0};
-    
-    for(int i=0;i<numsSize;i++) {
-        int idx=nums[i]/32;
-        int bit=nums[i]%32;
-        if (bitmap[idx]&(1 << bit))
-            res[(*returnSize)++] = nums[i];
-        else
-            bitmap[idx]|=(1<< bit);
+    for(int i=0;i<numsSize;i++){
+        int pos = abs(nums[i])-1;
+        if(nums[pos]<0)
+            res[(*returnSize)++]=pos+1;
+        else nums[pos]*=-1;
     }
     return res;
 }
