56ba760d9704acf610fa26ac1ffc491bfb3e96ab
Chirag K M <pes1ug23cs167@pesu.pes.edu>
2026-02-27 02:07:48 +0530

    Time: 7 ms (32.79%), Space: 24.5 MB (51.01%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.c
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.c
@@ -5,11 +5,17 @@ int* findDuplicates(int* nums, int numsSize, int* returnSize) {
     int* res =(int*)malloc(sizeof(int)*(numsSize/2 + 1));
     *returnSize=0;
 
+    //trying cyclic sorting
     for(int i=0;i<numsSize;i++){
-        int pos = abs(nums[i])-1;
-        if(nums[pos]<0)
-            res[(*returnSize)++]=pos+1;
-        else nums[pos]*=-1;
+        while(nums[i]!=nums[nums[i]-1]){
+            int t=nums[i];
+            nums[i]=nums[t-1];
+            nums[t-1]=t;
+        }
+    }
+    for(int i=0;i<numsSize;i++){
+        if(nums[i]!=i+1)
+            res[(*returnSize)++]=nums[i];
     }
     return res;
 }
