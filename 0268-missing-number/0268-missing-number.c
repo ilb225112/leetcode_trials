9e412fab86543aa83f6e650accecb83ca1290088
Chirag K M <pes1ug23cs167@pesu.pes.edu>
2026-03-01 00:20:15 +0530

    Time: 0 ms (100%), Space: 9.8 MB (5.1%) - LeetHub

--- 0268-missing-number/0268-missing-number.c
+++ 0268-missing-number/0268-missing-number.c
@@ -1,8 +1,6 @@
 int missingNumber(int* nums, int numsSize) {
-    int res=0;
-    for(int i=0;i<numsSize;i++){
-        res^=nums[i];
-        res^=(i+1);
-    }
-    return res;
+    int sum=(numsSize*(numsSize+1))/2;
+    for(int i=0;i<numsSize;i++)
+        sum-=nums[i];
+    return sum;
 }
