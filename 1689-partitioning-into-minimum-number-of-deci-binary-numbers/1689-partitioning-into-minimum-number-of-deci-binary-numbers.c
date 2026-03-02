128bcb67854532564e1b5f665f29872bcd24f1bc
Chirag K M <pes1ug23cs167@pesu.pes.edu>
2026-03-02 12:18:36 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.c
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.c
@@ -2,7 +2,9 @@ int minPartitions(char* n) {
     int res=0;
     int len= strlen(n);
 
-    for(int i=0;i<len;i++)
-        res=fmax(res,n[i]-'0');
+    for(int i=0;i<len;i++){
+        res=(n[i]-'0'>res)?n[i]-'0':res;
+        if(res==9) return 9;
+    }
     return res;
 }
