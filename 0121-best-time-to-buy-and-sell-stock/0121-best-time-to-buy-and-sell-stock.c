880f8794adc465c23e0c1c652aef081212157d43
Chirag K M <pes1ug23cs167@pesu.pes.edu>
2026-03-06 22:33:04 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.c
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.c
@@ -1,8 +1,11 @@
 int maxProfit(int* prices, int pricesSize) {
     int min=prices[0],profit=0;
     for(int i=1;i<pricesSize;i++){
-        profit = fmax(profit,prices[i]-min);
-        min=fmin(min,prices[i]);
+        profit+=min;
+        profit = (profit>prices[i])?profit:prices[i];
+        profit-=min;
+
+        min=(min<prices[i])?min:prices[i];
     }
     return profit;
 }
