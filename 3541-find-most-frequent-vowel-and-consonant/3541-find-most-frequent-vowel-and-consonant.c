3ff9fc152a19ec750a65393a8d8ad9ced01fada0
Chirag K M <pes1ug23cs167@pesu.pes.edu>
2026-03-11 00:26:33 +0530

    Time: 0 ms (100%), Space: 9.3 MB (12.3%) - LeetHub

---
+++ 3541-find-most-frequent-vowel-and-consonant/3541-find-most-frequent-vowel-and-consonant.c
@@ -0,0 +1,18 @@
+int maxFreqSum(char* s) {
+    int* cnt=calloc(26,sizeof(int));
+    int len=strlen(s);
+
+    for(int i=0;i<len;i++){
+        cnt[s[i]-'a']++;
+    }
+
+    int maxc=0,maxo=0;
+    for(int i=0;i<26;i++){
+        if(i==0||i==4||i==8||i==14||i==20)
+            maxo=(maxo+cnt[i]+abs(maxo-cnt[i]))/2;
+        else
+            maxc=(maxc+cnt[i]+abs(maxc-cnt[i]))/2;
+    }
+    free(cnt);
+    return maxc+maxo;
+}
