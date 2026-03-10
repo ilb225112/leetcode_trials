67965d1a8dac40e8d4f9d2f8244a0f5ed3079a4d
Chirag K M <pes1ug23cs167@pesu.pes.edu>
2026-03-10 17:52:57 +0530

    Time: 0 ms (100%), Space: 12.4 MB (10.34%) - LeetHub

--- 3668-restore-finishing-order/3668-restore-finishing-order.c
+++ 3668-restore-finishing-order/3668-restore-finishing-order.c
@@ -1,27 +1,19 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-bool bs(int* arr,int target,int n){
-    int l=0,r=n-1,mid;
-    while(l<=r){
-        mid=l+(r-l)/2;
-        if(arr[mid]==target)
-            return true;
-        else if(arr[mid]<target) 
-            l=mid+1;
-        else
-            r=mid-1;
-    }
-    return false;
-}
 
 int* recoverOrder(int* order, int orderSize, int* friends, int friendsSize, int* returnSize) {
     int* res=malloc(friendsSize*sizeof(int));
     *returnSize=0;
+    bool* check=calloc(orderSize+1,sizeof(bool));
+
+    for(int i=0;i<friendsSize;i++)
+        check[friends[i]]=1;
 
     for(int i=0;i<orderSize;i++){
-        if(bs(friends,order[i],friendsSize))
+        if(check[order[i]])
             res[(*returnSize)++]=order[i];
     }
+    free(check);
     return res;
 }
