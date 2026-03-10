8f27ab7ad13ab69a04b89d052a424b0085d2f7dc
Chirag K M <pes1ug23cs167@pesu.pes.edu>
2026-03-10 18:07:43 +0530

    Time: 0 ms (100%), Space: 8.4 MB (17.26%) - LeetHub

--- 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.c
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.c
@@ -1,9 +1,18 @@
-int xorOperation(int n, int start) {
-    int res=0;
+int xorTill(int x){
+    if(x%4==0) return x;
+    if(x%4==1) return 1;
+    if(x%4==2) return x+1;
+    return 0;
+}
 
-    for(int i=0;i<n;i++){
-        res^=start;
-        start+=2;
-    }
+int xorOperation(int n, int start) {
+    int s=start>>1;      
+    int e=s+n-1;
+
+    int res =xorTill(e)^xorTill(s-1);
+    res <<= 1;
+    if(start & 1 && n & 1) 
+        res |= 1;
+
     return res;
 }
