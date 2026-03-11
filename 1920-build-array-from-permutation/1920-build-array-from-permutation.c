2c9ada738b999d6424135a0c38bddae11f87b7b0
Chirag K M <pes1ug23cs167@pesu.pes.edu>
2026-03-12 01:14:10 +0530

    Time: 0 ms (100%), Space: 15.1 MB (15.38%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.c
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.c
@@ -2,10 +2,13 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* buildArray(int* nums, int numsSize, int* returnSize) {
-    int* res= malloc(numsSize*sizeof(int));
+    // store 2 vals: some multiple of number>n & old number as remainder
+    // so u later u can acess modified or ans using /
     *returnSize = numsSize;
 
     for(int i=0;i<numsSize;i++)
-        res[i]=nums[nums[i]];
-    return res;
+        nums[i] += (1001 * (nums[nums[i]] % 1001));
+    for(int i=0;i<numsSize;i++)
+        nums[i]/=1001;
+    return nums;
 }
