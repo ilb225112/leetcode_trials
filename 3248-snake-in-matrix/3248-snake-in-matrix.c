86305b69cd964cc4ba427f4f17f89c5c84a58ee0
Chirag K M <pes1ug23cs167@pesu.pes.edu>
2026-03-14 00:56:52 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3248-snake-in-matrix/3248-snake-in-matrix.c
+++ 3248-snake-in-matrix/3248-snake-in-matrix.c
@@ -1,10 +1,10 @@
 int finalPositionOfSnake(int n, char** commands, int commandsSize) {
-    int i=0,j=0;
+    int pos=0;
     for(int k=0;k<commandsSize;k++){
-        if(commands[k][0]=='R') j++;
-        else if(commands[k][0]=='L') j--;
-        else if(commands[k][0]=='U') i--;
-        else i++;
+        if(commands[k][0]=='R') pos +=1;
+        else if(commands[k][0]=='L') pos -=1;
+        else if(commands[k][0]=='U') pos -=n;
+        else pos +=n;
     }
-    return i*n+j;
+    return pos;
 }
