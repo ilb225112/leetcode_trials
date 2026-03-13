20bc1340e62de716898f2af18e80fdd5414dfed3
Chirag K M <pes1ug23cs167@pesu.pes.edu>
2026-03-14 00:23:21 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.c
+++ 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.c
@@ -1,12 +1,13 @@
 int countConsistentStrings(char * allowed, char ** words, int wordsSize){
-    int count=0;
-    bool isa[27]={0};
+    int count=0,mask=0;
+
     while(*allowed)
-        isa[*allowed++ -'a']=1;
+        mask|=1<<(*allowed++ -'a');
+
     for(int i=0;i<wordsSize;i++){
         int l1=strlen(words[i]),j=0;
         for(;j<l1;j++){
-            if(!isa[words[i][j]-'a'])
+            if(!(mask & (1 << (words[i][j]- 'a'))))
                 break;
         }
         if(j==l1) count++;
