655d03665a808e2b2c78a9204b0a0a1eda7834e1
Chirag K M <pes1ug23cs167@pesu.pes.edu>
2026-03-15 10:27:38 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.c
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.c
@@ -1,8 +1,18 @@
 bool arrayStringsAreEqual(char** word1, int word1Size, char** word2, int word2Size) {
-    char s1[1001]="",s2[1001]="";
-    for(int i=0;i<word1Size;i++)
-        strcat(s1,word1[i]);
-    for(int i=0;i<word2Size;i++)
-        strcat(s2,word2[i]);
-    return !strcmp(s1,s2);
+    int i=0,j=0;
+    int p=0,q=0;
+
+    while(i<word1Size && j<word2Size){
+        if(word1[i][p]!=word2[j][q])
+            return false;
+        p++;q++;
+
+        if(word1[i][p]=='\0'){
+            i++;p=0;
+        }
+        if(word2[j][q]=='\0'){
+            j++;q=0;
+        }
+    }
+    return (i==word1Size && j==word2Size);
 }
