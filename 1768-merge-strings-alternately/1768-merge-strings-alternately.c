8d1d82fa682bba92f89cbd75b3dc6d5484f2b7d6
Chirag K M <pes1ug23cs167@pesu.pes.edu>
2026-03-21 15:10:17 +0530

    Time: 2 ms (42.79%), Space: 8.8 MB (16.18%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.c
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.c
@@ -3,19 +3,11 @@ char * mergeAlternately(char * word1, char * word2){
     char* res=malloc(l1+l2+1);
     int idx=0;
 
-    while(*word1 && *word2){
-        res[idx++]=*word1;
-        res[idx++]=*word2;
-        word1++;word2++;
+    while(*word1 || *word2){
+        if(*word1) res[idx++] = *word1++;
+        if(*word2) res[idx++] = *word2++;
     }
-    while(*word1){
-        res[idx++]=*word1;
-        word1++;
-    }
-    while(*word2){
-        res[idx++]=*word2;
-        word2++;
-    }
-    res[idx]='\0';
+
+    res[idx] = '\0';
     return res;
 }
