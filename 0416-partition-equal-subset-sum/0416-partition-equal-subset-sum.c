e8fb9fe68dcf20522eaf3c8f31fca7af93b79066
Chirag K M <pes1ug23cs167@pesu.pes.edu>
2026-03-23 18:54:11 +0530

    Time: 45 ms (53.61%), Space: 9.1 MB (26.8%) - LeetHub

---
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.c
@@ -0,0 +1,21 @@
+bool canPartition(int* nums, int numsSize) {
+    int sum=0;
+    for(int i=0;i<numsSize;i++)
+        sum+=nums[i];
+    
+    if(sum & 1) return 0;
+
+    sum/=2;
+    bool* dp =calloc(sum+1,sizeof(bool));
+    dp[0] =true;
+    for (int i=0;i<numsSize;i++){
+        int num =nums[i];
+        for (int j=sum;j>=num;j--){
+            dp[j]=dp[j] || dp[j - num];
+        }
+    }
+    
+    bool res = dp[sum];
+    free(dp);
+    return res;
+}
