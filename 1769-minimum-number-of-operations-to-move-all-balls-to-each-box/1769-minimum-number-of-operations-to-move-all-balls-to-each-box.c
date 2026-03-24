e6b053c22e992d4e7c6f3df127933b6c45ec78bd
Chirag K M <pes1ug23cs167@pesu.pes.edu>
2026-03-24 20:20:49 +0530

    Time: 76 ms (44.35%), Space: 16.4 MB (28.7%) - LeetHub

---
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.c
@@ -0,0 +1,19 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* minOperations(char* boxes, int* returnSize) {
+    int n=strlen(boxes);
+    *returnSize =n;
+    int* res =malloc(n*sizeof(int));
+
+    for (int i=0;i<n;i++){
+        res[i] =0;
+        for (int j=0;j<n;j++){
+            if (boxes[j]=='1') {
+                res[i]+=abs(i-j);
+            }
+        }
+    }
+
+    return res;
+}
