b1c678a060158f0627615a04a6086df0b22d8569
Chirag K M <pes1ug23cs167@pesu.pes.edu>
2026-03-27 15:05:34 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.c
+++ 0412-fizz-buzz/0412-fizz-buzz.c
@@ -2,22 +2,31 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 char** fizzBuzz(int n, int* returnSize) {
-    char** res = calloc(n, sizeof(char*));
-    for(int i=0;i<n;i++){
-        if((i+1)%3==0 && (i+1)%5==0){
-            res[i]=malloc(sizeof(char)*10);
-            strcpy(res[i],"FizzBuzz");
-        }else if((i+1)%3==0){
-            res[i]=malloc(sizeof(char)*5);
-            strcpy(res[i],"Fizz");
-        }else if((i+1)%5==0){
-            res[i]=malloc(sizeof(char)*5);
-            strcpy(res[i],"Buzz");
-        }else{
-            res[i]=calloc(6,sizeof(char));
-            sprintf(res[i], "%d", i+1);
-        }
+    char** res = malloc(n * sizeof(char*));
+    *returnSize = n;
+
+    for(int i = 0; i < n; i++) {
+        res[i] = malloc(6);
+        sprintf(res[i], "%d", i + 1);
     }
-    *returnSize=n;
+
+    for(int i = 2; i < n; i += 3) {
+        free(res[i]);
+        res[i] = malloc(5);
+        strcpy(res[i], "Fizz");
+    }
+
+    for(int i = 4; i < n; i += 5) {
+        free(res[i]);
+        res[i] = malloc(5);
+        strcpy(res[i], "Buzz");
+    }
+
+    for(int i = 14; i < n; i += 15) {
+        free(res[i]);
+        res[i] = malloc(9);
+        strcpy(res[i], "FizzBuzz");
+    }
+
     return res;
 }
