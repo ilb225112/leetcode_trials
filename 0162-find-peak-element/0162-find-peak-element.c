8d943f5dba2c6957790c27e11024faaccabeebca
Chirag K M <pes1ug23cs167@pesu.pes.edu>
2026-04-01 17:04:14 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.c
+++ 0162-find-peak-element/0162-find-peak-element.c
@@ -1,17 +1,12 @@
 int findPeakElement(int* nums, int numsSize) {
-    if(numsSize==1) return 0;
-    if(nums[0]>nums[1]) return 0;
-    if(nums[numsSize-1]>nums[numsSize-2]) return numsSize-1;
+    int l=0,r=numsSize-1;
 
-    int l=1,r=numsSize-2;
-    while(l<=r){
+    while (l<r){
         int mid=l+((r-l)>>1);
-        if(nums[mid]>nums[mid+1] && nums[mid]>nums[mid-1])
-            return mid;
-        else if(nums[mid]>nums[mid-1])
+        if (nums[mid]<nums[mid+1])
             l=mid+1;
         else
-            r=mid-1;
+            r=mid;
     }
-    return -1;
+    return l;
 }
