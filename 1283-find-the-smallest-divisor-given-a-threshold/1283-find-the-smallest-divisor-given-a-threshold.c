79293e70c3c966c1eec249f6ee54b75028eb0d57
Chirag K M <pes1ug23cs167@pesu.pes.edu>
2026-04-03 21:21:21 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.c
+++ 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.c
@@ -1,18 +1,15 @@
-bool lt(int* nums, int numsSize,int div,int threshold){
-    long res=0;
-    for(int i=0;i<numsSize;i++)
-        res+=(nums[i]+div-1)/div;
-    return res<=threshold;
-}
-
 int smallestDivisor(int* nums, int numsSize, int threshold) {
     int l=1,r=nums[0];
     for(int i=1;i<numsSize;i++)
         if(nums[i]>r) r=nums[i];
 
     while(l<=r){
-        int mid=l+((r-l)>>1);
-        if(lt(nums,numsSize,mid,threshold))
+        int mid=l+((r-l)>>1),sum=0;
+        for(int i=0;i<numsSize;i++){
+            sum+=(nums[i]+mid-1)/mid;
+            if(sum>threshold) break;
+        }
+        if(sum<=threshold)
             r=mid-1;
         else 
             l=mid+1;
