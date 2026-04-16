f9d0614f9418cdd1361e194f800976f91afb8c28
Chirag K M <pes1ug23cs167@pesu.pes.edu>
2026-04-16 22:51:26 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3898-find-the-degree-of-each-vertex/3898-find-the-degree-of-each-vertex.c
+++ 3898-find-the-degree-of-each-vertex/3898-find-the-degree-of-each-vertex.c
@@ -2,14 +2,14 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* findDegrees(int** matrix, int matrixSize, int* matrixColSize, int* returnSize) {
-    int* res=malloc(sizeof(int)*matrixSize);
+    int* res=calloc(matrixSize,sizeof(int));
     *returnSize=matrixSize;
 
-    for(int i=0;i<matrixSize;i++){
-        int r=0;
-        for(int j=0;j<matrixSize;j++)
-            r+=matrix[i][j];
-        res[i]=r;
-    }
+    for(int i=0;i<matrixSize;i++)
+        for(int j=i+1;j<matrixSize;j++){
+            res[i]+=matrix[i][j];
+            res[j]+=matrix[i][j];
+        }
+    
     return res;
 }
