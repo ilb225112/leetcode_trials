548cb6ca3394c977160eaeca0d9a55f085af6fc5
Chirag K M <pes1ug23cs167@pesu.pes.edu>
2026-04-18 13:57:46 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1637-widest-vertical-area-between-two-points-containing-no-points/1637-widest-vertical-area-between-two-points-containing-no-points.c
+++ 1637-widest-vertical-area-between-two-points-containing-no-points/1637-widest-vertical-area-between-two-points-containing-no-points.c
@@ -1,8 +1,8 @@
 int cmp(const void*a, const void* b){
-    int* p1=*(int**)a;
-    int* p2=*(int**)b;
-
-    if(p1[0]>p2[0]) return 1;
+    int x1=(*(int**)a)[0];
+    int x2=(*(int**)b)[0];
+    
+    if(x1>x2) return 1;
     return -1;
 }
 
@@ -11,7 +11,9 @@ int maxWidthOfVerticalArea(int** points, int pointsSize, int* pointsColSize) {
     int res=0;
 
     for(int i=1;i<pointsSize;i++){
-        res=fmax(res,points[i][0]-points[i-1][0]);
+        int diff=points[i][0]-points[i-1][0];
+        if(diff>res) 
+            res=diff;     
     }
     return res;
 }
