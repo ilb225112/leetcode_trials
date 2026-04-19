789299cbe8219f0535868c6754c363cfd6add2c8
Chirag K M <pes1ug23cs167@pesu.pes.edu>
2026-04-19 22:22:24 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2859-sum-of-values-at-indices-with-k-set-bits/2859-sum-of-values-at-indices-with-k-set-bits.c
+++ 2859-sum-of-values-at-indices-with-k-set-bits/2859-sum-of-values-at-indices-with-k-set-bits.c
@@ -1,9 +1,14 @@
 int sumIndicesWithKSetBits(int* nums, int numsSize, int k) {
     int sum=0;
+    if(k==0) return nums[0];
 
-    for(int i=0;i<numsSize;i++){
-        if(k==__builtin_popcount(i))
-            sum+=nums[i];
+    unsigned int i=(1U<<k)-1;
+    while(i<numsSize){
+        sum+=nums[i];
+        unsigned int c=i & -i;
+        unsigned int r=i+c;
+        i=(((r^i)>>2)/c) |r;
     }
     return sum;
+    
 }
