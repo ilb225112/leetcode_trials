ae927645c5c9f451235c74229c9889877648cea2
Chirag K M <pes1ug23cs167@pesu.pes.edu>
2026-04-23 22:13:36 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.c
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.c
@@ -1,23 +1,11 @@
-bool binS(int* nums,int numsSize,int target){
-    int l=0,r=numsSize-1;
-
-    while(l<=r){
-        int mid=l+((r-l)>>1);
-        if(nums[mid]==target)
-            return 1;
-        else if(nums[mid]<target)
-            l=mid+1;
-        else
-            r=mid-1;
-    }
-    return 0;
-}
-
 int arithmeticTriplets(int* nums, int numsSize, int diff) {
-    int counter=0;
-    for(int i=0;i<numsSize;i++){
-        if(binS(nums,numsSize,nums[i]+diff) && binS(nums,numsSize,nums[i]+2*diff))
-            counter++;
-    }
-    return counter;
+    bool isPresent[260]={0};
+    int res=0;
+
+    for(int i=0;i<numsSize;i++)
+        isPresent[nums[i]]=1;
+    for(int i=0;i<numsSize;i++)
+        res+= (isPresent[nums[i]+diff] && isPresent[nums[i]+2*diff]);
+
+    return res;
 }
