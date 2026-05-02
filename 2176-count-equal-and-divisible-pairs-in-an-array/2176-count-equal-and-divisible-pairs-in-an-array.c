3831008027023de4803df99fa6f382179f490d97
Chirag K M <pes1ug23cs167@pesu.pes.edu>
2026-05-02 11:50:00 +0530

    Time: 7 ms (3.77%), Space: 8.8 MB (99.06%) - LeetHub

---
+++ 2176-count-equal-and-divisible-pairs-in-an-array/2176-count-equal-and-divisible-pairs-in-an-array.c
@@ -0,0 +1,8 @@
+int countPairs(int* nums, int n, int k) {
+    int res=0;
+    for(int i=0;i<n;i++)
+        for(int j=i+1;j<n;j++)
+            if(nums[i]==nums[j] && (i*j)%k  == 0)
+                res++;
+    return res;
+}
