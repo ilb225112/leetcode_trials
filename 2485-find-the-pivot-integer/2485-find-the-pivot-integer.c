37e18e9abca2d68a1275b1767a9f7692ed5e5223
Chirag K M <pes1ug23cs167@pesu.pes.edu>
2026-05-03 13:43:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.c
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.c
@@ -1,21 +1,7 @@
-int sum(int x){
-    return x*(x+1)>>1;
-}
-
 int pivotInteger(int n) {
-    int tsum=sum(n);
-    int l=1,r=n;
-    while(l<=r){
-        int mid=l+((r-l)>>1);
-        int temp=sum(mid);
-        int rsum=tsum-temp+mid;
-
-        if(temp==rsum)
-            return mid;
-        if(temp>rsum)
-            r=mid-1;
-        else
-            l=mid+1;
-    }
+    int tsum=n*(n+1)>>1;
+    int x=sqrt(tsum);
+    if(x*x==tsum)
+        return x;
     return -1;
 }
