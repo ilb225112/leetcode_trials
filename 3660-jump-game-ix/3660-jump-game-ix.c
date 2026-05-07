fe2af84eca804673e7271f5b6a7b5524ebfb4d7c
Chirag K M <pes1ug23cs167@pesu.pes.edu>
2026-05-07 14:11:01 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3660-jump-game-ix/3660-jump-game-ix.c
+++ 3660-jump-game-ix/3660-jump-game-ix.c
@@ -5,25 +5,21 @@ int* maxValue(int* nums, int numsSize, int* returnSize) {
     *returnSize=numsSize;
     if(numsSize==0) return NULL;
 
-    int* pre=malloc(sizeof(int)*numsSize);
     int* suf=malloc(sizeof(int)*numsSize);
     int* res=malloc(sizeof(int)*numsSize);
 
-    pre[0]=nums[0];
-    for(int i=1;i<numsSize;i++)
-        pre[i]=fmax(pre[i-1],nums[i]);
-
     suf[numsSize-1]=nums[numsSize-1];
     for(int i=numsSize-2;i>=0;i--)
-        suf[i]=fmin(suf[i+1],nums[i]);
+        suf[i]=(nums[i]<suf[i+1])?nums[i]:suf[i+1];
 
-    res[numsSize-1]=pre[numsSize-1];
-    for(int i=numsSize-2;i>=0;i--){
-        if(pre[i]>suf[i+1])
+    res[0] = nums[0];
+    for (int i=1;i<numsSize;i++) 
+        res[i] =(res[i-1]>nums[i])?res[i-1]:nums[i];
+
+    for(int i=numsSize-2;i>=0;i--)
+        if(res[i]>suf[i+1])
             res[i]=res[i+1];
-        else
-            res[i]=pre[i];
-    }
 
+    free(suf);
     return res;
 }
