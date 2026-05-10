ab5117c8c998ee73dd20d078149f2adf4f402ccb
Chirag K M <pes1ug23cs167@pesu.pes.edu>
2026-05-10 17:56:25 +0530

    Time: 0 ms (100%), Space: 8.4 MB (79.63%) - LeetHub

---
+++ 3894-traffic-signal-color/3894-traffic-signal-color.c
@@ -0,0 +1,6 @@
+char* trafficSignal(int timer) {
+    if(timer==0) return "Green";
+    else if(timer==30) return "Orange";
+    else if(timer<=90 && timer>30) return "Red";
+    return "Invalid";
+}
