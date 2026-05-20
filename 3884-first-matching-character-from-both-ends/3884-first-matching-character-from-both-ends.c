55a11cefbe85639aa269fc5fff7fb05664eee9ae
Chirag K M <pes1ug23cs167@pesu.pes.edu>
2026-05-20 09:41:52 +0530

    Time: 0 ms (100%), Space: 9 MB (27.82%) - LeetHub

--- 3884-first-matching-character-from-both-ends/3884-first-matching-character-from-both-ends.c
+++ 3884-first-matching-character-from-both-ends/3884-first-matching-character-from-both-ends.c
@@ -1,9 +1,13 @@
 int firstMatchingIndex(char* s) {
-    int len=strlen(s);
-    int l1=len>>1;
+    char* st=s;
+    char *end=s;
+    while(*end) end++;
+    end--;
 
-    for(int i=0;i<l1;i++){
-        if(s[i]==s[len-i-1]) return i;
+    while(st<=end){
+        if(*st==*end) return st-s;
+        st++;
+        end--;
     }
-    return (len&1)?l1:-1;
+    return -1;
 }
