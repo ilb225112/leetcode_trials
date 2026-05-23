2d72b823e5ad1b1fee511d3a3c090c94de6523e3
Chirag K M <pes1ug23cs167@pesu.pes.edu>
2026-05-23 15:46:38 +0530

    Time: 9 ms (69.57%), Space: 9.5 MB (33.33%) - LeetHub

--- 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.c
+++ 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.c
@@ -2,9 +2,8 @@
 
 int minOperations(int* nums, int numsSize){
     int res=0;
-    if(numsSize<=1) return res;
-
     int prev=nums[0];
+    
     for(int i=1;i<numsSize;i++){
             prev=max(nums[i],prev+1);
             res+=prev-nums[i];
