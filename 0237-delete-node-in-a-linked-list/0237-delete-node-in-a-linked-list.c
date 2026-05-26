27ac085ce1f8fd82b1d5cd0321b82bb4ec6259bb
Chirag K M <pes1ug23cs167@pesu.pes.edu>
2026-05-26 17:26:49 +0530

    Time: 8 ms (36.45%), Space: 11.6 MB (29.56%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.c
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.c
@@ -7,10 +7,8 @@
  */
 void deleteNode(struct ListNode* node) {
     struct ListNode* temp=node->next;
-    
+
     node->val=temp->val;
     node->next=temp->next;
     free(temp);
-
-    return;
 }
