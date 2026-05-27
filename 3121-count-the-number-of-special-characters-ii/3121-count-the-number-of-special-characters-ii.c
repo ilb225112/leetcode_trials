60b06bc95a9d47ffc39839566162cabdd872d231
Chirag K M <pes1ug23cs167@pesu.pes.edu>
2026-05-27 17:51:57 +0530

    Time: 15 ms (91.95%), Space: 13.3 MB (96.55%) - LeetHub

--- 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.c
+++ 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.c
@@ -1,24 +1,17 @@
-#define FOR(i) for(int i=0;i<26;i++)
-
 int numberOfSpecialChars(char* word) {
-    int fidx[26];
-    int lidx[26];
-    int count=0;
+    int caps=0;
+    int small=0;
+    int invalid=0;
 
-    FOR(i){
-        fidx[i]=-1;
-        lidx[i]=-1;
+    while(*word){
+        char ch=*word;
+
+        if(ch<='Z') caps|=(1<<(ch-'A'));
+        else 
+            if(caps &(1<<(ch-'a'))) invalid|=(1<<(ch-'a'));
+            else 
+                small|=(1<<(ch-'a'));  
+        word++;
     }
-
-    int len=strlen(word);
-    for(int i=0;i<len;i++){
-        char ch=word[i];
-        if(ch<='Z'&& fidx[ch-'A']==-1) fidx[ch-'A']=i; 
-        else if(ch>='a') lidx[ch-'a']=i; 
-    }
-
-    FOR(i)
-        count+=(lidx[i]!=-1 && fidx[i]!=-1 && lidx[i]<fidx[i]);
-
-    return count;
+    return __builtin_popcount(caps&small&(~invalid));
 }
