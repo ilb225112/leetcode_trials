6857f2a8a83712e2b9998c3e6a9ec4da96564d40
Chirag K M <pes1ug23cs167@pesu.pes.edu>
2026-05-30 11:57:57 +0530

    Time: 0 ms (100%), Space: 9.5 MB (98.63%) - LeetHub

--- 2843-count-symmetric-integers/2843-count-symmetric-integers.c
+++ 2843-count-symmetric-integers/2843-count-symmetric-integers.c
@@ -1,16 +1,19 @@
 int countSymmetricIntegers(int low, int high) {
     int res=0;
-    for(int i=low;i<=high;i++){
-        if(i<=100) {
-            if(i%11==0) {res++;i+=10;}
-            else i=i+(11-i%11-1);
-        }
-        else if(i<1000) i=1000;
-        else if(i>=1000){
-            int rsum=(i%10) + (i%100)/10;
-            int lsum=(i%1000)/100 + i/1000;
+    int i=low;
+
+    int s2=(low<11)?11 :((low+10)/11)*11;
+    int e2=(high>99)?99:high;
+
+    if(s2<=e2) res+=(e2-s2)/11 +1;
+    int s4=(low<1000)?1000:low;
+
+    for(int i=s4;i<=high;i++){
+            int left = i / 100;
+            int right = i % 100;
+            int lsum = (left / 10) + (left % 10);
+            int rsum = (right / 10) + (right % 10);
             if(rsum==lsum) res++;
-        }
     }
     return res;
 }
