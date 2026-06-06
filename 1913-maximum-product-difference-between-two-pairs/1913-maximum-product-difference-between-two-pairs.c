05fca6ae86f0e1a9824a2d1f056a1ed4155732d1
Chirag K M <pes1ug23cs167@pesu.pes.edu>
2026-06-06 11:57:13 +0530

    Time: 13 ms (94.44%), Space: 10 MB (72.22%) - LeetHub

--- 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.c
+++ 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.c
@@ -1,21 +1,31 @@
 int maxProductDifference(int* nums, int numsSize){
-    int min1=INT_MAX,min2=INT_MAX;
-    int max1=0,max2=0;
-
-    for(int i=0;i<numsSize;i++){
-        int n = nums[i];
-        if(n<min1){
-            min2=min1;
-            min1=n;
-        }else if(n<min2)
-            min2=n;
-
-        if(n>max1){
-            max2=max1;
-            max1=n;
-        }else if(n>max2)
-            max2=n;
+    int min1,min2,max1,max2;
+    if(nums[0]<nums[1]){
+        min1=nums[0]; min2=nums[1];
+        max1=nums[1]; max2=nums[0];
+    }else{
+        min1=nums[1]; min2=nums[0];
+        max1=nums[0]; max2=nums[1];
     }
     
-    return (max2*max1)-(min2*min1);
+    for(int i=2;i<numsSize;i++){
+        int n=nums[i];
+        if (n<min2) {
+            if(n<min1) {
+                min2=min1;
+                min1=n;
+            }else 
+                min2=n;
+        }
+
+        if(n>max2){
+            if(n>max1) {
+                max2=max1;
+                max1=n;
+            }else
+                max2=n;
+        }
+    }
+
+    return (max1*max2)-(min1*min2);
 }
