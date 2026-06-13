29465f160296bbebdc33c40c5979190e30b7a273
Chirag K M <pes1ug23cs167@pesu.pes.edu>
2026-06-13 15:04:37 +0530

    Time: 0 ms (100%), Space: 9.1 MB (21.75%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.c
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.c
@@ -1,13 +1,12 @@
 bool judgeCircle(char* moves) {
+    int dx[4]={0,-1,1,0};
+    int dy[4]={1,0,0,-1};
     int x=0,y=0;
+
     while(*moves){
-        if(*moves &1) y++;
-        else{
-            char ch=*moves;
-            if(ch==68) y--;
-            else if(ch==76) x--;
-            else x++;
-        }
+        int idx=*moves % 5;
+        x+=dx[idx];
+        y+=dy[idx];
         moves++;
     }
     return !(x|y);
