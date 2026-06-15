f82343f08ece1e4cd06e4eaa7ccb3f48577708f7
Chirag K M <pes1ug23cs167@pesu.pes.edu>
2026-06-15 08:45:59 +0530

    Time: 0 ms (100%), Space: 83.1 MB (53.83%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.c
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.c
@@ -8,14 +8,14 @@
 struct ListNode* deleteMiddle(struct ListNode* head) {
     if(head==NULL || head->next==NULL) return NULL;
 
-    struct ListNode* fast=head->next->next, *slow=head,*temp;
-    while(fast && fast->next)
-        fast=fast->next->next,slow=slow->next;
-    temp=slow->next;
-    slow->next=temp->next;
-
-    temp->next=NULL;
-    free(temp);
+    struct ListNode* fast=head, *slow=head,*prev=NULL;
+    while(fast && fast->next){
+        prev=slow;
+        slow=slow->next;
+        fast=fast->next->next;
+    }
+    prev->next=slow->next;
+    free(slow);
 
     return head;
 }
