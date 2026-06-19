d96f413469d1b46b1312dea2a545105500f05ece
Chirag K M <pes1ug23cs167@pesu.pes.edu>
2026-06-19 07:40:28 +0530

    Time: 0 ms (100%), Space: 8.6 MB (3.67%) - LeetHub

--- 2427-number-of-common-factors/2427-number-of-common-factors.c
+++ 2427-number-of-common-factors/2427-number-of-common-factors.c
@@ -1,12 +1,20 @@
+int gcd(int a,int b){
+    while(b!=0){
+        int temp=b;
+        b=a%b;
+        a=temp;
+    }
+    return a;
+}
+
 int commonFactors(int a, int b) {
     int count=0;
-    if(a>b) return commonFactors(b,a);
+    int g=gcd(a,b);
 
-    for(int i=1;i*i<=a;i++){
-        if(a%i==0){
-            count += (b%i==0);
-            int temp=a/i;
-            if(temp!=i && b%temp==0) count++;
+    for(int i=1;i*i<=g;i++){
+        if(g%i==0){
+            count ++;
+            if(i*i !=g) count++;
         }
     }
 
