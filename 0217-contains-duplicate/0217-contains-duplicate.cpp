46279b98e7a1ddf6cf6073076d37c31df137c7d9
Chirag K M <pes1ug23cs167@pesu.pes.edu>
2026-06-28 17:19:29 +0530

    Time: 78 ms (11.83%), Space: 111.1 MB (5.39%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>& v) {
         unordered_set<int> s;
-        for(int val: v) s.insert(val);
-        return s.size()<v.size();
+        for(int val: v) 
+            if(!s.insert(val).second) return 1;
+        return 0;
     }
 };
