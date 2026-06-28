9771b8ee694163df2008823c09254207bd59a45c
Chirag K M <pes1ug23cs167@pesu.pes.edu>
2026-06-28 17:37:29 +0530

    Time: 0 ms (100%), Space: 40.2 MB (80.99%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -2,24 +2,18 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& v) {
         int len=v.size(),temp=1;
-        vector<int> prefix(len);
-        vector<int> suffix(len);
         vector<int> res(len);
         
         for(int i=0;i<len;i++){
-            prefix[i]=temp;
+            res[i]=temp;
             temp*=v[i];
         }
 
         temp=1;
         for(int i=len-1;i>=0;i--){
-            suffix[i]=temp;
+            res[i]*=temp;
             temp*=v[i];
-        }
-
-        for(int i=0;i<len;i++)
-            res[i]=prefix[i]*suffix[i];
-        
+        }     
         return res;
     }
 };
