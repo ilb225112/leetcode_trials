9e3e570a397cffb928ec5f283ebaf9b931eb09f8
Chirag K M <pes1ug23cs167@pesu.pes.edu>
2026-07-07 15:22:32 +0530

    Time: 0 ms (100%), Space: 8.2 MB (53.19%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
+        if(s.length() != goal.length()) return 0;
         string srch=goal+goal;
         if(srch.contains(s)) return 1;
         return 0;
