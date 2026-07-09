4690030b2c5c7db29b6db8777a233e6f4b802450
Chirag K M <pes1ug23cs167@pesu.pes.edu>
2026-07-09 09:51:34 +0530

    Time: 47 ms (43.16%), Space: 88.8 MB (7.56%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        map<int,int> uniq;
+        int n=fruits.size();
+        int l=0,r=0,maxlen=0;
+
+        while(r<n){
+            uniq[fruits[r]]++;
+            if(uniq.size()>2){
+                uniq[fruits[l]]--;
+                if(uniq[fruits[l]]==0)
+                    uniq.erase(fruits[l]);
+                l++;
+            }
+            if(uniq.size()<=2)
+                maxlen=max(r-l+1,maxlen);
+            r++;
+        }
+        
+        return maxlen;
+    }
+};
