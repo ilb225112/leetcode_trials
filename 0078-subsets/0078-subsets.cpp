947b72c03b4ac4b2d908cbfa33009206dd53e21e
Chirag K M <pes1ug23cs167@pesu.pes.edu>
2026-07-09 16:42:25 +0530

    Time: 0 ms (100%), Space: 9.9 MB (82.32%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,21 +1,15 @@
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        int npos=(1<<nums.size());
-        int temp=0;
-        vector<vector<int>> res(npos);
-        int idx=0;
+        vector<vector<int>> res={{}};
+        res.reserve(1<<nums.size());
 
-        while(temp<npos){
-            int ttemp=temp,i=0;
-            while(ttemp){
-                if(ttemp&1)
-                    res[idx].push_back(nums[i]);
-                ttemp>>=1;
-                i++;
+        for(int num:nums){
+            int n=res.size();
+            for(int i=0;i<n;i++){
+                res.push_back(res[i]);
+                res.back().push_back(num);
             }
-            temp++;
-            idx++;
         }
         return res;
     }
