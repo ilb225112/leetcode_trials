455e481382e5e9a40a493bbb30307f82cd3497ea
Chirag K M <pes1ug23cs167@pesu.pes.edu>
2026-07-13 23:36:35 +0530

    Time: 0 ms (100%), Space: 11.5 MB (78.98%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int l=0,r=nums.size()-1;
+        while(l<=r && nums[l]==0) l++;
+        while(r>=l && nums[r]==2) r--;
+        int travel=l;
+
+        while(travel<=r){
+            if(nums[travel]==0){
+                swap(nums[travel],nums[l]);
+                l++;
+                travel++;
+            }else if(nums[travel]==2){
+                if(nums[r]==2){r--;continue;}
+                swap(nums[travel],nums[r]);
+                r--;
+            }else
+                travel++;
+        }
+    }
+};
