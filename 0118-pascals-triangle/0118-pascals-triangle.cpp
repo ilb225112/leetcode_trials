ca5b0ee1196dbb7ed7f44e367aa1bad647c708f5
Chirag K M <pes1ug23cs167@pesu.pes.edu>
2026-07-13 22:40:43 +0530

    Time: 0 ms (100%), Space: 9.7 MB (63.65%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -3,9 +3,11 @@ public:
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> ptri(numRows);
         for(int i=0;i<numRows;i++){
-            for(int j=0;j<=i;j++){
-                if(i==j || j==0) ptri[i].push_back(1);
-                else ptri[i].push_back(ptri[i-1][j]+ptri[i-1][j-1]);
+            ptri[i].resize(i+1);
+            ptri[i][0]=1;
+            ptri[i][i]=1;
+            for(int j=1;j<i;j++){
+                ptri[i][j]=ptri[i-1][j]+ptri[i-1][j-1];
             }
         }
         return ptri;
