43a7a4e15d8a7df46bf3db03102fbbbba2b0b9f6
Chirag K M <pes1ug23cs167@pesu.pes.edu>
2026-07-14 16:54:00 +0530

    Time: 0 ms (100%), Space: 28.2 MB (68.42%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -3,10 +3,8 @@ public:
     int majorityElement(vector<int>& nums) {
         int cnt=0,num1;
         for(int i:nums){
-            if(cnt==0){
-                num1=i;
-                cnt=1;
-            }else if(i==num1) cnt++;
+            if(cnt==0) num1=i;
+            if(i==num1) cnt++;
             else cnt--;
         }
         return num1;
