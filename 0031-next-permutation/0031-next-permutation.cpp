2329b548019dcaeabca85c75946665feac2b623b
Chirag K M <pes1ug23cs167@pesu.pes.edu>
2026-07-14 10:32:51 +0530

    Time: 0 ms (100%), Space: 15.9 MB (7.95%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,6 +1,34 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
+        int n=nums.size();
+        if(n<3){
+            reverse(nums.begin(),nums.end());
+            return;
+        }
+        int idx=-1;
+
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                idx=i;
+                break;
+            }
+        }
+        if(idx==-1){
+            reverse(nums.begin(),nums.end());
+            return;
+        }
+
+        int swpidx=n-1;
+        int ele=nums[idx];
+        for(int i=n-1;i>idx;i--){
+            if(nums[i]>ele){
+                swpidx=i;
+                break;
+            }
+        }
+
+        swap(nums[swpidx],nums[idx]);
+        reverse(nums.begin()+idx+1,nums.end());
     }
 };
