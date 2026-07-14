4395bbca7cc62d3bd2efc47312a3e425669ba2e7
Chirag K M <pes1ug23cs167@pesu.pes.edu>
2026-07-14 10:46:09 +0530

    Time: 0 ms (100%), Space: 10 MB (93.86%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        int m=matrix.size(),n=matrix[0].size();
+        int m=matrix.size();
+        int n=matrix[0].size();
+
         for(int i=0;i<m;i++){
             for(int j=i+1;j<n;j++){
                 swap(matrix[i][j],matrix[j][i]);
             }
         }
-
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n/2;j++)
-                swap(matrix[i][j],matrix[i][n-j-1]);
+        
+        int hcol=n>>1;
+        for(int j=0;j<hcol;j++){
+            for(int i=0;i<m;i++){
+                swap(matrix[i][j], matrix[i][n-j-1]);
+            }
         }
+        
     }
 };
