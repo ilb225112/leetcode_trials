d2744844e7e038c8feb8a0b72c2a49f457385d8a
Chirag K M <pes1ug23cs167@pesu.pes.edu>
2026-07-14 17:17:46 +0530

    Time: 2 ms (20.83%), Space: 9.5 MB (5.12%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int paths(int i,int j,int m,int n,vector<vector<int>>& dp){
+        if(i>=m || j>=n) return 0;
+        if(i==m-1 && j==n-1) return 1;
+        if(dp[i][j]) return dp[i][j];
+        int ans=paths(i+1,j,m,n,dp)+paths(i,j+1,m,n,dp);
+        dp[i][j]=ans;
+        return ans;
+    }
+    int uniquePaths(int m, int n) {
+        if(m==1 || n==1) return 1;
+        vector<vector<int>> dp(m,vector<int>(n,0)); 
+        paths(0,0,m,n,dp);
+        return dp[0][0];
+    }
+};
