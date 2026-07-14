f10ef63347277d66e44ab809921d0f44c78eb6a7
Chirag K M <pes1ug23cs167@pesu.pes.edu>
2026-07-14 17:18:53 +0530

    Time: 0 ms (100%), Space: 9.5 MB (30.34%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -4,9 +4,7 @@ public:
         if(i>=m || j>=n) return 0;
         if(i==m-1 && j==n-1) return 1;
         if(dp[i][j]) return dp[i][j];
-        int ans=paths(i+1,j,m,n,dp)+paths(i,j+1,m,n,dp);
-        dp[i][j]=ans;
-        return ans;
+        return dp[i][j]=paths(i+1,j,m,n,dp)+paths(i,j+1,m,n,dp);   
     }
     int uniquePaths(int m, int n) {
         if(m==1 || n==1) return 1;
