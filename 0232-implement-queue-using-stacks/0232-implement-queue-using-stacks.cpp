6dce598c5d0bfdbc3dc9c8de48dc24b0f3721cfb
Chirag K M <pes1ug23cs167@pesu.pes.edu>
2026-07-16 15:04:45 +0530

    Time: 0 ms (100%), Space: 10.3 MB (6.68%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -6,33 +6,29 @@ public:
     }
     
     void push(int x) {
+        while(s1.size()){
+            s2.push(s1.top());
+            s1.pop();
+        }
         s1.push(x);
+        while(s2.size()){
+            s1.push(s2.top());
+            s2.pop();
+        }
     }
     
     int pop() {
-        if(s2.empty()){
-           while(s1.size()){
-                s2.push(s1.top());
-                s1.pop();
-            }
-        }
-        int ele=s2.top();
-        s2.pop();
+        int ele=s1.top();
+        s1.pop();
         return ele;
     }
     
     int peek() {
-        if(s2.empty()){
-            while(s1.size()){
-                s2.push(s1.top());
-                s1.pop();
-            }
-        }
-        return s2.top();
+        return s1.top();
     }
     
     bool empty() {
-        return s1.empty() && s2.empty();
+        return s1.empty();
     }
 };
 
