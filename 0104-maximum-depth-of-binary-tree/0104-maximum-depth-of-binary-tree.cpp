18e70a3b5918ef15198bc73d79fb257d9e2eeeea
Chirag K M <pes1ug23cs167@pesu.pes.edu>
2026-07-28 16:36:31 +0530

    Time: 0 ms (100%), Space: 22.1 MB (38.7%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -13,8 +13,8 @@ class Solution {
 public:
     int maxDepth(TreeNode* root) {
         if(!root) return 0;
-        int left=maxDepth(root->left);
-        int right=maxDepth(root->right);
-        return max(left,right)+1;
+        int left=maxDepth(root->left)+1;
+        int right=maxDepth(root->right)+1;
+        return max(left,right);
     }
 };
