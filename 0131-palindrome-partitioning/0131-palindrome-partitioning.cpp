635c43393728735b57a79bb49dd2e4cef65aaea3
Chirag K M <pes1ug23cs167@pesu.pes.edu>
2026-07-30 15:19:33 +0530

    Time: 112 ms (11.92%), Space: 131.9 MB (11.28%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,14 +1,6 @@
 class Solution {
 public:
-    bool isPalindrome(string s,int i, int j){
-        while(i<j){
-            if(s[i]!=s[j]) return 0;
-            i++;j--;
-        }
-        return 1;
-    }
-
-    void part(string s, vector<vector<string>> &res, vector<string> &temp, int idx,int start){
+    void part(string s, vector<vector<string>> &res, vector<string> &temp, int idx,int start,vector<vector<bool>> &dp){
         int n=s.size();
         if(idx==n){
             if(start==idx)
@@ -16,11 +8,11 @@ public:
             return;
         }
 
-        part(s,res,temp,idx+1,start);
+        part(s,res,temp,idx+1,start,dp);
 
-        if(isPalindrome(s,start,idx)){
+        if(dp[start][idx]){
             temp.push_back(s.substr(start,idx-start+1));
-            part(s,res,temp,idx+1,idx+1);
+            part(s,res,temp,idx+1,idx+1,dp);
             temp.pop_back();
         }
     }
@@ -28,7 +20,17 @@ public:
     vector<vector<string>> partition(string s) {
         vector<vector<string>> res;
         vector<string> temp;
-        part(s,res,temp,0,0);
+        int n=s.size();
+        vector<vector<bool>> dp(n,vector<bool>(n,0));
+        for(int i=n-1;i>=0;i--){
+            for(int j=i;j<n;j++){
+                if(s[i]==s[j]){
+                    if(j-i<=1) dp[i][j]=1;
+                    else dp[i][j]=dp[i+1][j-1];
+                }
+            }
+        }
+        part(s,res,temp,0,0,dp);
         return res;
     }
 };
