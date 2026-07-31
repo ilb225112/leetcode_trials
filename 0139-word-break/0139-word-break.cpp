1fc2cacfc819b8bf92c040cdf76338c6b41e0ad6
Chirag K M <pes1ug23cs167@pesu.pes.edu>
2026-07-31 14:32:32 +0530

    Time: 3 ms (71.62%), Space: 13.1 MB (60.81%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,17 +1,37 @@
 class Solution {
 public:
+    struct TrieNode{
+        struct TrieNode* children[26]={};
+        bool isEnd;
+    };
+    TrieNode* root;
+
+    void insert(string word){
+        TrieNode* node=root;
+        for(char ch: word){
+            int idx=ch-'a';
+            if(node->children[idx]==NULL) node->children[idx]=new TrieNode();
+            node=node->children[idx];
+        }
+        node->isEnd=1;
+    }
+
     bool wordBreak(string s, vector<string>& wordDict) {
-        unordered_set<string> dict(wordDict.begin(), wordDict.end());
+        root=new TrieNode();
         int n=s.size();
         vector<bool> dp(n+1,0);
         dp[0]=1;
 
-        for(int i=1;i<=n;i++){
-            for(int j=0;j<i;j++){
-                if(dp[j] && dict.count(s.substr(j, i-j))){
-                    dp[i]=true;
-                    break;
-                }
+        for(string w: wordDict) insert(w);
+
+        for(int i=0;i<n;i++){
+            if(!dp[i]) continue;
+            TrieNode* node=root;
+            for(int j=i;j<n;j++){
+                int idx=s[j]-'a';
+                if(!node->children[idx]) break;
+                node=node->children[idx];
+                if(node->isEnd) dp[j+1]=1;
             }
         }
         return dp[n];
