414379de1531629d0d7057edc9f0653f324e1682
Chirag K M <pes1ug23cs167@pesu.pes.edu>
2026-08-06 14:50:13 +0530

    Time: 0 ms (100%), Space: 9.7 MB (82.39%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.cpp
+++ 0686-repeated-string-match/0686-repeated-string-match.cpp
@@ -1,18 +1,31 @@
 class Solution {
 public:
-    int repeatedStringMatch(string a, string b) {
-        int s1=a.size();
-        int s2=b.size();
-        if(a==b) return 1;
-        
-        string ta=a,tb=b;
-        int tsa=s1;
-        while(s2+tsa>s1) {ta+=a;s1+=tsa;}
-        size_t index=ta.find(b);
-        if(index==string::npos) return -1;
-        int totalNeeded = index + s2;
-        int count = (totalNeeded + tsa - 1) / tsa;
-        return count;
+    void buildLps(const string &s,vector<int> &lps,int m){
+        int i=1,len=0;
+        while(i<m){
+            if(s[i]==s[len]){
+                lps[i++]=++len;
+            }else if(len!=0){
+                len=lps[len-1];
+            }else
+                lps[i++]=0;
+        }
+    }
 
+    int repeatedStringMatch(string a, string b) {
+        int m=b.size(),n=a.size();
+        vector<int> lps(m,0);
+        buildLps(b,lps,m);
+
+        int j=0;
+        int minCount=(n+m-1)/n;
+        int limit=(minCount+1)*n;
+        for(int pos=0;pos<limit;pos++){
+            char ch=a[pos%n];
+            while(j>0 && ch!=b[j]) j=lps[j-1];
+            if(b[j]==ch) j++;
+            if(j==m) return pos/n +1;
+        }
+        return -1;
     }
 };
