9373100ae8a9ed614d742ef06819a459b65ae3a9
Chirag K M <pes1ug23cs167@pesu.pes.edu>
2026-08-06 15:21:37 +0530

    Time: 3 ms (80.93%), Space: 10.2 MB (28.47%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string RLE(string s){
+        int count=0;
+        string res="";
+        int n=s.size();
+
+        char ch=s[0];
+        for(int i=0;i<n;i++){
+            if(ch==s[i]) count++;
+            else{
+                res+=to_string(count);
+                res+=ch;
+                count=0;
+                ch=s[i];
+                i--;
+            }
+        }
+        res+=to_string(count);
+        res+=ch;
+
+        return res;
+    }
+
+    string countAndSay(int n) {
+        if(n==1) return "1";
+        return RLE(countAndSay(n-1));
+    }
+};
