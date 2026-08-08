10c07db877e6ebf8798f601e81f9139c225955d5
Chirag K M <pes1ug23cs167@pesu.pes.edu>
2026-08-08 12:05:01 +0530

    Time: 0 ms (100%), Space: 7.8 MB (74.62%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -3,11 +3,12 @@ public:
     int climbStairs(int n){
         if(n==0) return 0;
         if(n<=2) return n;
-        vector<int> dp(n+1,-1);
-        dp[0]=dp[1]=1;
-        dp[2]=2;
-        for(int i=3;i<=n;i++)
-            dp[i]=dp[i-1]+dp[i-2];
-        return dp[n];
+        int curr=2,prev1=1,prev2=1;
+
+        for(int i=3;i<=n;i++){
+            prev2=prev1,prev1=curr;
+            curr=prev2+prev1;
+        }
+        return curr;
     }
 };
