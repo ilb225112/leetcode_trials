f3a794cd062ed5736ccf3003ab605a199054ee4c
Chirag K M <pes1ug23cs167@pesu.pes.edu>
2026-08-08 12:03:27 +0530

    Time: 2 ms (5.05%), Space: 8.5 MB (39.03%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
-    int climbStair(int n,vector<int>& dp) {
-        if(n<=1) return 1;
-        if(n==2) return 2;
-        if(dp[n]!=-1) return dp[n];
-        int oneStep=climbStair(n-1,dp);
-        int twoStep=climbStair(n-2,dp);
-        return dp[n]=oneStep+twoStep;
-    }
-
     int climbStairs(int n){
+        if(n==0) return 0;
+        if(n<=2) return n;
         vector<int> dp(n+1,-1);
-        return climbStair(n,dp);
+        dp[0]=dp[1]=1;
+        dp[2]=2;
+        for(int i=3;i<=n;i++)
+            dp[i]=dp[i-1]+dp[i-2];
+        return dp[n];
     }
 };
