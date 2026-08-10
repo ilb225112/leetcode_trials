7ba4d6670ae098916f43bee8d3e54079e85ff0e7
Chirag K M <pes1ug23cs167@pesu.pes.edu>
2026-08-10 14:23:51 +0530

    Time: 79 ms (35.24%), Space: 164.7 MB (17.77%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,40 +1,24 @@
 class Solution {
 public:
-    string preprocess(const string& s){
-        string t="^#";
-        for(char ch: s){
-            t+=ch;
-            t+='#';
-        }
-        t+='$';
-        return t;
+    int expandAroundCenter(string s,int left,int right){
+        while(left>=0 && right<s.length() && s[left]==s[right]) left--,right++;
+        return right-left-1;
     }
 
     string longestPalindrome(string s) {
-        if(s.size()==1) return s;
-        string t=preprocess(s);
-        int n=t.size();
-        vector<int> p(n,0);
+        if(s.empty()) return "";
+        int start=0,maxLen=1;
+        int n=s.size();
 
-        int center=0,right=0;
-        int maxLen=0,maxCenter=0;
-
-        for(int i=1;i<n;i++){
-            int mirror=2*center-i;
-            if(i<right) p[i]=min(p[mirror],right-i);
-            while(t[i+1+p[i]]==t[i-1-p[i]]) p[i]++;
-
-            if(i+p[i]>right){
-                center=i;
-                right=i+p[i];
-            }
-
-            if(p[i]>maxLen){
-                maxLen=p[i];
-                maxCenter=i;
+        for(int i=0;i<n;i++){
+            int len1=expandAroundCenter(s,i,i);
+            int len2=expandAroundCenter(s,i,i+1);
+            int len=max(len1,len2);
+            if(len>maxLen){
+                maxLen=len;
+                start=i-(len-1)/2;
             }
         }
-        int start=(maxCenter-maxLen)>>1;
         return s.substr(start,maxLen);
     }
 };
