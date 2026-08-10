7db5708acc22c101aaa5fc76b6e8376c2a40171b
Chirag K M <pes1ug23cs167@pesu.pes.edu>
2026-08-10 14:28:19 +0530

    Time: 0 ms (100%), Space: 14.9 MB (23.68%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,21 +1,13 @@
 class Solution {
 public:
-    #define FOR(i,a,n) for(int i=a;i<n;i++)
-    //2.Better Approach : O(nlogn)
-    vector<int> twoSum(vector<int>& arr, int target) {
-        int arrSize=arr.size();
-        vector<int> res(2,-1);
-        unordered_map<int,int> idx;
-        
-        FOR(i,0,arrSize){
-            int temp=target-arr[i];
-            if(idx.find(temp) !=idx.end()){
-                res[0]=idx[temp];
-                res[1]=i;
-                break;
-            }
-            idx[arr[i]]=i;
-        }
-        return res;
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> req;
+        int len=nums.size();
+        for(int i=0;i<len;i++){
+            int required=target-nums[i];
+            if(req.find(required)!=req.end()) return {i,req[required]};
+            req[nums[i]]=i;
+        } 
+        return {};
     }
 };
