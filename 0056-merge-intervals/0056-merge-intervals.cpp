23ab76c5c36c43751d1941d5ee3edc503a43b0e7
Chirag K M <pes1ug23cs167@pesu.pes.edu>
2026-08-10 14:32:46 +0530

    Time: 4 ms (66.12%), Space: 23.8 MB (82.66%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,30 +1,20 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        int m=intervals.size();
-        if(m<2) return intervals;
+        if(intervals.empty()) return {};
 
         sort(intervals.begin(),intervals.end());
-        
-        vector<vector<int>> res;
-        res.push_back(intervals[0]);
+        vector<vector<int>> merged;
+        merged.push_back(intervals[0]);
 
-        for(int i=1;i<m;i++){
-            int end=res.back()[1];
-            int j=i;
-            
-            while(j<m && end>=intervals[j][0]){
-                end=max(end,intervals[j][1]);
-                j++;
-            }
-            
-            if(i==j)
-                res.push_back(intervals[i]);
-            else{
-                res.back()[1]=end;
-                i=j-1;
+        int len=intervals.size();
+        for(int i=1;i<len;i++){
+            if(merged.back()[1]>=intervals[i][0]){
+                merged.back()[1]=max(intervals[i][1],merged.back()[1]);
+            }else{
+                merged.push_back(intervals[i]);
             }
         }
-        return res;
+        return merged;
     }
 };
